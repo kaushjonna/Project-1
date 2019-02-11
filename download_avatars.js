1ce90e00546ddb6da4c655ad1cfb8e34f4d69a83
Kaush <kaush.jonna@gmail.com>
2019-02-11 16:57:54 -0500

    set up Main functions

--- download_avatars.js
+++ download_avatars.js
@@ -2,9 +2,20 @@ const request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {}
+function getRepoContributors(repoOwner, repoName, cb) {
+  const options = {
+    url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+    headers: {
+      'User-Agent': 'GithubAvatarApp',
+      Authorization: 'token d4042bc13aaca7fd5b2d18476d2f3510a62ad5f5',
+    },
+  };
+  request(options, (err, res, body) => {
+    cb(err, body);
+  });
+}
 
 getRepoContributors('jquery', 'jquery', (err, result) => {
   console.log('Errors:', err);
-  console.log('Result:', result);
+  console.log('Result:', JSON.parse(result));
 });
